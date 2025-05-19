da9c51eae673aa687b631e3a5b19d7a318b49374
Yulien21 <mauriciocrak86@gmail.com>
2025-05-18 21:50:01 -0300

    TP3.C

--- TP3.C
+++ TP3.C
@@ -1,16 +1,37 @@
-# include < stdio .h >
-int main ( void )
-{
-int num;
-printf (" Ingrese un n´umero entero : ") ;
-scanf (" %d", & num ) ;
-if ( num % 5 == 0)
-{
-printf ("El n´umero %d es m´ultiplo de 5\n", num ) ;
-}
-else
-{
-printf ("El n´umero %d no es m´ultiplo de 5\n", num) ;
-}
-return 0;
+#include <stdio.h>
+
+int main(void) {
+	
+	float peso;
+	float altura;
+	float IMC;
+	
+	printf("Ingrese su peso en kg");
+	scanf("%f",&peso);
+	
+	printf("Ingrese su altura en metros");
+	scanf("%f",&altura);
+	
+	if (altura>0){
+		
+		IMC = peso/(altura*altura);
+		
+		printf("Su IMC es: %.2f\n",IMC);
+		
+		printf("Tabla de referencia del IMC:\n");
+		//Clasificación
+		if (IMC < 18.5) {
+			printf("Clasificación: Bajo peso\n");
+		} else if (IMC >= 18.5 && IMC <= 24.9) {
+			printf("Clasificación: Normal\n");
+		} else if (IMC >= 25.0 && IMC <= 29.9) {
+			printf("Clasificación: Sobrepeso\n");
+		} else if (IMC >29.9){
+			printf("Clasificación: Obesidad\n");
+		  
+		}
+
+	}
+	
+	return 0;
 }
