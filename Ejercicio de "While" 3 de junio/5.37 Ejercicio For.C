0ddd7707f0a169766b3ee8d8b7f4e5e1afd72690
Yulien21 <mauriciocrak86@gmail.com>
2025-06-16 21:58:10 -0300

    5.37 Ejercicio For.C
    Realizar un programa que imprima los números desde el 5 hasta el 0 y luego
    vuelva hasta el 5 como en el siguiente ejemplo (se debe utilizar al menos una
    estructura for)

---
+++ Ejercicio de "While" 3 de junio/5.37 Ejercicio For.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	
+	for(int i = 5; i >= 0; i--){
+		
+	printf("%d\n",i);
+	}
+	for(int f = 1; f <= 5;f++){
+		
+		printf("%d\n",f);
+	}
+	
+	return 0;
+}
+
